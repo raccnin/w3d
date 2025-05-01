5257aa6ee270cd1c858753823138b50a696d3d1d
raccnin <johncw004@gmail.com>
2025-05-01 18:28:59 +0100

    added models

--- w3d/static/js/model_loader.js
+++ w3d/static/js/model_loader.js
@@ -1,32 +1,88 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const width = window.innerWidth, height = window.innerHeight;
-
-// init
-
-const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-camera.position.z = 1;
+const canvas = document.getElementById('threeCanvas');
+const canvasContainer = document.getElementById('canvasContainer');
+var width = canvasContainer.offsetWidth - 40, height = window.innerHeight - 180;
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(toHex(drink_data['colour']));
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+const camera = new THREE.PerspectiveCamera(75, width/height , 0.1, 1000);
+camera.position.y = 0;
+camera.position.z = 4;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(width, height);
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height );
-renderer.setAnimationLoop( animate );
-document.getElementById('canvas').appendChild( renderer.domElement );
+window.addEventListener('resize', onResize, false);
 
-// animation
+const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate( time ) {
+// button functionality
+const wireframeToggle = document.getElementById("wireframeToggle");
+wireframeToggle.addEventListener('click', toggleWireframe, false);
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+var rotating = true;
+const rotateToggle = document.getElementById("rotationToggle");
+rotateToggle.addEventListener('click', () => {rotating = !rotating})
 
-	renderer.render( scene, camera );
+const loader = new GLTFLoader();
 
-}anvas.appendChild(renderer.domElement)
+
+loader.load(drink_data['model_path'], 
+	function (gltf){
+		let model_scene = gltf.scene;
+		scene.add(model_scene);
+	}, 
+	undefined, 
+	function (error) {
+		console.error(error)
+	})
+
+var lights = [];
+const directionalLight = new THREE.DirectionalLight();
+directionalLight.position.set(0, 1, 2);
+
+const ambientLight = new THREE.HemisphereLight(0xffffff);
+
+lights.push(directionalLight, ambientLight);
+
+lights.forEach((light) => {scene.add(light)})
+
+function animate(){
+	
+	// perform transforms
+	scene.traverse((child) => {
+		if (child instanceof THREE.Mesh){
+			if (rotating) {
+				child.rotateY(0.01);
+			}
+		}
+	})
+
+	renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+
+function onResize() {
+	width = canvasContainer.offsetWidth;
+	height = canvasContainer.offsetHeight;
+	renderer.setSize(width, height);
+	camera.aspect = width/height;
+	camera.updateProjectionMatrix();
+}
+
+function toHex(s) {
+	return parseInt(s, 16);
+}
+
+function toggleWireframe() {
+	scene.traverse((child) => {
+		if (child instanceof(THREE.Mesh)){
+			child.material.wireframe = !child.material.wireframe;
+		}
+	})
+}
